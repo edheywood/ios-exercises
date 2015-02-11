11119fb5854ca6336827271698ef3b8ad92e78e3
edheywood <edward@waffleapp.co>
2015-02-11 19:22:45 +0000

    BlocExercises/BlocExercises/WizardOfOzTests.m

--- BlocExercises/BlocExercises/WizardOfOzTests.m
+++ BlocExercises/BlocExercises/WizardOfOzTests.m
@@ -35,12 +35,12 @@
 {
     BOOL colorSwitches = [self.wonderfulWizard switchesFromBlackAndWhiteToColor];
     XCTAssertTrue(colorSwitches == YES, @"The color switches from black and white to color, but the `switchesFromBlackAndWhiteToColor` method indicated that it doesn't.");
-}
+	
 
 - (void)testTheMainCharacterIsCorrect
 {
     NSString *mainCharacter = [self.wonderfulWizard mainCharacter];
-   XCTAssertEqualObjects(mainCharacter, @"John", @"Incorrect main character string returned.");
+   XCTAssertEqualObjects(mainCharacter, @"Dorothy", @"Incorrect main character string returned.");
 }
 
 @end
