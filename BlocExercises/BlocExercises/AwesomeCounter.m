56623ada642ffc82968a0d28bff1d507ad1e6f39
edheywood <edward@waffleapp.co>
2015-03-03 20:20:10 +0000

    BlocExercises/BlocExercises/AwesomeCounter.m

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -6,13 +6,66 @@
 //
 //
 
+//#import "AwesomeCounter.h"
+//
+//@implementation AwesomeCounter
+//
+//- (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
+//    NSInteger newNumber = 1;
+//    for (NSInteger two = 2; two < 1; two++) {
+//        newNumber = newNumber + two;
+//    }
+//    
+//    return @"%i";
+//}
+//
+//@end
+
+//
+//#import "AwesomeCounter.h"
+//
+//@implementation AwesomeCounter
+//
+//- (NSString *)stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber:(NSInteger)otherNumber
+//{
+//    NSString *resultString = @"";
+//    
+//    while (number <= otherNumber) {
+//        resultString = [NSString stringWithFormat:@"%@%ld", resultString, (long)number];
+//        number++;
+//    }
+//    
+//    return resultString;
+//}
+//
+//@end
+
 #import "AwesomeCounter.h"
 
 @implementation AwesomeCounter
 
-- (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+- (NSString *)stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber:(NSInteger)otherNumber
+{
+    NSString *resultString = @"";
+    
+    if (number >= otherNumber)
+    {
+        while (number >= otherNumber)
+        {
+            resultString = [NSString stringWithFormat:@"%ld%@", (long)number, resultString];
+            number--;
+        }
+    }
+    else
+    {
+        while (number <= otherNumber)
+        {
+            resultString = [NSString stringWithFormat:@"%@%ld", resultString, (long)number];
+            number++;
+        }
+    }
+    
+    return resultString;
 }
 
 @end
