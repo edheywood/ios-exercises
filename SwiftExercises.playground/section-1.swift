0c8d2afc9b537c93463dc5b041112b29acbb2da7
edheywood <edward@waffleapp.co>
2015-03-10 13:24:43 +0000

    Completed Swift Exercises

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -7,12 +7,14 @@ Strings
 */
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
-    // WORK HERE
+    
+    
     return cheese
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
 // Make fullSentence say "My favorite cheese is cheddar."
+println("My favouite cheese is \(fullSentence)")
 
 /*
 
@@ -20,11 +22,11 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+let numberArray = [1, 2, 3, 4, 5]
 // Add 5 to this array
 // WORK HERE
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
 
@@ -37,7 +39,19 @@ Loops
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
 
+for numbers in 1...10
+{
+    println(numbers)
+}
+
 // Use a half-closed range loop to print 1 - 10, inclusively
+
+for numbers in 1..<10
+{
+    println(numbers)
+}
+
+
 // WORK HERE
 
 let worf = [
@@ -56,12 +70,13 @@ let picard = [
 let characters = [worf, picard]
 
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
+    
+    var favouriteDrinks = ["prune juice", "tea", "beer"]
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
 }
 
-let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
+var favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
 
 favoriteDrinks
 
@@ -71,10 +86,20 @@ Functions
 
 */
 
+
 // Make a function that inputs an array of strings and outputs the strings separated by a semicolon
 
-let strings = ["milk", "eggs", "bread", "challah"]
+func groceries(list: String) -> String {
 
+let strings = ["milk", "eggs", "bread", "challah"]
+    
+    let strings = list
+    
+    let semicolon = ";".join(strings)
+    
+    return semicolon
+}
+    
 // WORK HERE - make your function and pass `strings` in
 
 let expectedOutput = "milk;eggs;bread;challah"
@@ -89,3 +114,4 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+let loveCereal = sorted(cerealArray, <)
